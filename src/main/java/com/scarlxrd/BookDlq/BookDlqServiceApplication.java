c20ce5b4f19919b82a27aeda7f2401a50a7e38e0
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-09-21 21:47:48 -0300

    feat: database config

---
+++ src/main/java/com/scarlxrd/BookDlq/BookDlqServiceApplication.java
@@ -0,0 +1,13 @@
+package com.scarlxrd.BookDlq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDlqServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookDlqServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scarlxrd/BookDlq/BookDlqServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scarlxrd.BookDlq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookDlqServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
