25dc3702823e7c3cf9e610a18caf2f07d033ba80
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-09-21 21:53:37 -0300

    feat: creation of dto sent via rabbitmq payload.

---
+++ src/main/java/com/scarlxrd/BookDlq/model/entity/BookRequestDTO.java
@@ -0,0 +1,4 @@
+package com.scarlxrd.BookDlq.model.entity;
+
+public record BookRequestDTO(String title, String author,String isbn) {
+}
---
+++ src/main/java/com/scarlxrd/BookDlq/model/entity/ClientRequestDTO.java
@@ -0,0 +1,6 @@
+package com.scarlxrd.BookDlq.model.entity;
+
+import java.util.List;
+
+public record ClientRequestDTO(String name, String lastName, String cpfNumber, List<BookRequestDTO> books) {
+}
